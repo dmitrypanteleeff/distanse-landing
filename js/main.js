513b30db83db420493d11d3741e51a1a0d70333a
Dmitry <DmitryPanteleeff@yandex.ru>
2020-06-10 18:22:53 +0300

    сделал анимацию гамбургера и затемнил фон

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const menuBtn = document.querySelector(".header-block__m-menu");
 const menuSidebar = document.querySelector(".main__menu-sidebar");
+const darkFon = document.querySelector(".main__feed-fon");
+const line1 = document.querySelector(".line__line-1");
+const line2 = document.querySelector(".line__line-2");
+const line3 = document.querySelector(".line__line-3");
 menuBtn.addEventListener("click",function(){
     menuSidebar.classList.toggle("main__menu-sidebar--active");
+    darkFon.classList.toggle("main__feed-fon--active");
+    line1.classList.toggle("line__line-1--active");
+    line2.classList.toggle("line__line-2--active");
+    line3.classList.toggle("line__line-3--active");
+    console.log("проверка");
 });
